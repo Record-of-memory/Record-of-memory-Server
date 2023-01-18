ed6ab3fc7c73ee091d3e88f169fcae954819a63e
Gyehwan Baek <b99hwan@gmail.com>
2023-01-18 23:10:52 +0900

    [FEAT]: Comment Entity 개발

--- src/main/java/com/rom/domain/comment/domain/Comment.java
+++ src/main/java/com/rom/domain/comment/domain/Comment.java
@@ -1,21 +1,39 @@
 package com.rom.domain.comment.domain;
 
 import com.rom.domain.common.BaseEntity;
+import com.rom.domain.diary.domain.Diary;
 import com.rom.domain.user.domain.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
 
-@Entity
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Entity
 public class Comment extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+
+    @Builder
+    public Comment(Long id, String content, User user, Diary diary) {
+        this.id = id;
+        this.content = content;
+        this.user = user;
+        this.diary = diary;
+    }
 }
