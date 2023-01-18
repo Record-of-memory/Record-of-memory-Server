e614f65b88e1109a39531bd59c4a418ab2a81301
Gyehwan Baek <b99hwan@gmail.com>
2023-01-18 22:28:44 +0900

    feat: PR#2

--- src/main/java/com/rom/domain/comment/domain/Comment.java
+++ src/main/java/com/rom/domain/comment/domain/Comment.java
@@ -1,4 +1,21 @@
 package com.rom.domain.comment.domain;
 
-public class Comment {
+import com.rom.domain.common.BaseEntity;
+import com.rom.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
