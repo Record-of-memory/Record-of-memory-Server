7695ce59aa3b2138ddb07889046a6492821cf0a1
yoonjeongin <yoonjeongin@naver.com>
2023-01-22 20:18:56 +0900

    [FEAT]: Record Entity 구현

--- src/main/java/com/rom/domain/record/domain/Record.java
+++ src/main/java/com/rom/domain/record/domain/Record.java
@@ -1,12 +1,14 @@
 package com.rom.domain.record.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.rom.domain.diary.domain.Diary;
+import com.rom.domain.user.domain.User;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,4 +18,40 @@ public class Record {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Date date;
+
+    private String title;
+
+    private String content;
+
+    // 이미지
+    @Column(name="img_url")
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_book_id")
+    private Diary diary;
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus recordStatus;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist // DB에 insert 되기 직전에 실행
+    public void created_at(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate  // update 되기 직전 실행
+    public void updated_at() { this.updatedAt = LocalDateTime.now(); }
 }
---
+++ src/main/java/com/rom/domain/record/domain/RecordStatus.java
@@ -0,0 +1,5 @@
+package com.rom.domain.record.domain;
+
+public enum RecordStatus {
+    active, deleted
+}
