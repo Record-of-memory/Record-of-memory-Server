3feb0271c5f198d3d200134db79e2386881e5091
박세진 <jinpark99@naver.com>
2023-01-31 02:42:48 +0900

    [FEAT]: 다이어리ID로 유저 조회 API 구현

--- src/main/java/com/rom/domain/diary/presentation/DiaryController.java
+++ src/main/java/com/rom/domain/diary/presentation/DiaryController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Diaries", description = "Diaries API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/diaries")
+@RequestMapping("/api/diaries")
 public class DiaryController {
 
     private final DiaryService diaryService;
@@ -52,6 +52,18 @@ public class DiaryController {
             @Parameter(description = "AccessToken을 입력해주세요", required = true) @CurrentUser UserPrincipal userPrincipal
     ){
         return diaryService.findDiariesByUserId(userPrincipal);
+    }
+
+    @Operation(summary = "다이어리 유저 목록 조회", description = "다이어리의 유저 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "다이어리의 유저 목록 조회 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = DiaryDetailRes.class))}),
+            @ApiResponse(responseCode = "400", description = "다이어리의 유저 목록 조회 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})
+    })
+    @GetMapping("{diaryId}")
+    public ResponseEntity<?> findUsersByDiary(
+            @Parameter(description = "Diary ID를 입력해주세요", required = true) @PathVariable Long diaryId
+    ){
+        return diaryService.findUsersByDiaryId(diaryId);
     }
 
     @Operation(summary = "이메일로 유저 다이어리 추가", description = "이메일로 유저를 다이어리에 추가합니다.")
