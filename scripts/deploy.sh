c601d169ea9e5d53ede96c6aee7966c42ff0fd72
Chae Jieun <86587287+cje1903@users.noreply.github.com>
2023-02-06 00:08:50 +0900

    [FIX] test deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,9 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-# log를 저장할 파일 생성
-APP_LOG=$REPOSITORY/application.log
-ERROR_LOG=$REPOSITORY/error.log
-DEPLOY_LOG=$REPOSITORY/deploy.log
-
 # 1) 애플리케이션이 구동중인지 확인하기 위한 애플리케이션 이름, jar 파일의 이름
 APP_NAME=RecordOfMemory
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep ‘.jar’ | tail -n 1)
+# JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep ‘.jar’ | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 # 2) 현재 인스턴스에서 애플리케이션이 구동중인지 확인, 구동중이면 종료
@@ -27,4 +22,4 @@ fi
 
 # 3) jar 파일을 배포
 echo “> $JAR_PATH 배포”
-nohup java -jar $JAR_PATH > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar RecordOfMemory-0.0.1-SNAPSHOT.jar &
