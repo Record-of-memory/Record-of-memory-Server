cc9be778f970d3534857b677dab50d29fdd4feaf
Sejin Park <95167215+sejineer@users.noreply.github.com>
2023-02-14 12:18:34 +0900

    [FIX]: 날짜로 일기 찾기 API 수정 완료 (#50)

--- src/main/java/com/rom/domain/record/domain/repository/RecordRepository.java
+++ src/main/java/com/rom/domain/record/domain/repository/RecordRepository.java
@@ -4,6 +4,7 @@ import com.rom.domain.diary.domain.Diary;
 import com.rom.domain.record.domain.Record;
 import com.rom.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +14,9 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
     List<Record> findAllByDiary(Diary diary);
     List<Record> findAllByDiaryAndDate(Diary diary, Date date);
     List<Record> findAllByDiaryAndUser(Diary diary, Optional<User> user);
+
+    @Query("SELECT r FROM Record r WHERE r.diary.status = 'ACTIVE' AND r.date = :date AND r.user = :user")
+    List<Record> findAllByDateAndUser(Date date, User user);
 
     List<Record> findAllByDiaryAndUserAndImgUrlNotNull(Diary diary, User user);
 
