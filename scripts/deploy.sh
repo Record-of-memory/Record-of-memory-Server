5decedf0a35acd05cf694553e0ec8a4c1e2b0edf
Chae Jieun <86587287+cje1903@users.noreply.github.com>
2023-02-15 05:39:11 +0900

    [FIX] " 에러 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,9 +13,9 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then 
-	echo “> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다.”
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-	echo “> kill -15 $CURRENT_PID”
+	echo "> kill -15 $CURRENT_PID"
 	sudo kill -15 $CURRENT_PID
 	sleep 5
 fi
@@ -26,6 +26,6 @@ fi
 #sudo chown ubuntu app
 
 # 4) jar 파일을 배포
-echo “> $JAR_PATH 배포”
+echo "> $JAR_PATH 배포"
 # nohup java -jar $JAR_PATH &
 nohup java -jar $JAR_PATH > /dev/null 2>&1 &
