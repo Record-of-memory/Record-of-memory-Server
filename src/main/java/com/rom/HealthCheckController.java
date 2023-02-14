95e5177b31e73554e08cf27eda4ca1cedb02302d
cje1903 <cje1903@naver.com>
2023-02-15 04:30:45 +0900

    [FEAT] add controller for healthcheck

---
+++ src/main/java/com/rom/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.rom;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping(value = "/healthcheck")
+    public String healthcheck(){
+        return "OK";
+    }
+
+}
