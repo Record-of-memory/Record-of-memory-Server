a20f9c75ccba03f75c556a4f974dfbebdae4ecf0
박세진 <jinpark99@naver.com>
2023-02-14 12:17:51 +0900

    [FIX]: 날짜로 일기 찾기 API 수정 완료

---
+++ src/main/java/com/rom/domain/record/dto/FindRecordByDateRes.java
@@ -0,0 +1,24 @@
+package com.rom.domain.record.dto;
+
+import com.rom.domain.diary.dto.DiaryRecordDetailRes;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FindRecordByDateRes {
+
+    private Long diaryId;
+
+    private String diaryName;
+
+    private List<DiaryRecordDetailRes> records;
+
+    @Builder
+    public FindRecordByDateRes(Long diaryId, String diaryName, List<DiaryRecordDetailRes> records) {
+        this.diaryId = diaryId;
+        this.diaryName = diaryName;
+        this.records = records;
+    }
+}
