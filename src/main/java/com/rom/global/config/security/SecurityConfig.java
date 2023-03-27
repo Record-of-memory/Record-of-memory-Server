ec9da1717ca4c08f3ab62283413800feec6f4b9f
RealWorldd <jinpark99@naver.com>
2023-03-27 14:16:30 +0900

    [FIX]: SecurityConfig 수정

--- src/main/java/com/rom/global/config/security/SecurityConfig.java
+++ src/main/java/com/rom/global/config/security/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                         .permitAll()
                     .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
                         .permitAll()
-                    .requestMatchers("/login/**", "/oauth2/**", "/oauth2/**")
+                    .requestMatchers("/login/**", "/oauth2/**", "/auth/**")
                         .permitAll()
                     .requestMatchers("/blog/**")
                         .permitAll()
